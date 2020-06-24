35f183d2b9e25cd1c45a544de8e6c11a47e6ca35
Mohammed Ramadan <mohammed.ramadan@aurea.com>
2020-06-24 19:47:00 +0200

    initial setup for Unit Testing

---
+++ src/index.test.js
@@ -0,0 +1,19 @@
+import {expect} from 'chai';
+import {JSDOM} from 'jsdom';
+import fs from 'fs';
+
+describe('Setup tests', ()=>{
+   it('should pass', ()=>{
+       expect(true).to.equal(true);
+   })
+});
+
+describe('index.html test', ()=>{
+    it('should say js web kit', ()=>{
+        const index = fs.readFileSync('./src/index.html', 'utf-8');
+        const DOM = new JSDOM(index);
+        const h1 = DOM.window.document.getElementsByTagName('h1')[0];
+        expect(h1.innerHTML.toLowerCase()).to.equal('js web kit');
+        DOM.window.close();
+    })
+})
